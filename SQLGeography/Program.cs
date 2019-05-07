660bc453c57f1d84b9e9f49079fcdf779b3b77dc
SAID-PC-O <SAID-PC-O@DESKTOP-R2Q5MLK>
2019-05-07 08:27:55 +0400

    Add project files.

---
+++ SQLGeography/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQLGeography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
