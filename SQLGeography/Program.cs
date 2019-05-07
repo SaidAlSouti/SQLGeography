57c710740d858a44402e64094ccba7daff000079
SAID-PC-O <SAID-PC-O@DESKTOP-R2Q5MLK>
2019-05-07 12:44:18 +0400

    f

--- SQLGeography/Program.cs
+++ SQLGeography/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace SQLGeography
 {
@@ -7,6 +10,17 @@ namespace SQLGeography
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            IEnumerable<Spatial> spatials = new List<Spatial>();
+            using (var connection = new SqlConnection("Server=.;Database=SpatialTest;Trusted_Connection=True;"))
+            {
+                spatials = connection.Query<Spatial>(
+                    "[dbo].[Spatial_Get]",
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ SQLGeography/Spatial.cs
@@ -0,0 +1,13 @@
+﻿using GeoAPI.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLGeography
+{
+    public class Spatial
+    {
+        public int Id { set; get; }
+        public IPoint Geo { set; get; }
+    }
+}
